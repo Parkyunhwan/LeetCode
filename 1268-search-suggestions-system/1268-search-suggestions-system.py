8bcc6436663c4979b717358ae24d1c188d1afb8a
YunHwan Park <47476276+Parkyunhwan@users.noreply.github.com>
2023-01-25 22:46:53 +0900

    Time: 72 ms (72.65%), Space: 15.4 MB (96.58%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -6,11 +6,12 @@ class Solution(object):
         result = []
         # 2. 모든 검색글자에 대해 최대3개까지 조건에 맞는 단어를 보여줌
         currSearchWord = ""
+        idx = 0
         for ch in searchWord:
             currList = []
             currSearchWord += ch
             # 정렬된 리스트에서 현재 검색글자가 들어갈만한 위치를 bisect로 구한다.
-            idx = bisect.bisect_left(products, currSearchWord) # idx까지가 검색가능한 단어의 첫 시작 idx라고 볼수 있다.
+            idx = bisect.bisect_left(products, currSearchWord, idx) # idx까지가 검색가능한 단어의 첫 시작 idx라고 볼수 있다.
             for p in products[idx:idx + 3]:
                 if p.startswith(currSearchWord):
                     currList.append(p)
