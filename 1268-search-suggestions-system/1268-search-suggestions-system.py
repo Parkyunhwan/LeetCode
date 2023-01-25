b0501f28060c2eb2b911ee10d117d4d6b8bdb057
YunHwan Park <47476276+Parkyunhwan@users.noreply.github.com>
2023-01-25 22:45:10 +0900

    Time: 65 ms (77.78%), Space: 15.6 MB (44.44%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,23 +1,21 @@
+import bisect
 class Solution(object):
     def suggestedProducts(self, products, searchWord):
+        # 1. 정렬을 하면 검색을 단순화할 수 있다. (검색안되는 것 이후의 것은 모두 검색안됨)
         products = sorted(products)
         result = []
-        sw = ""
-        for i in range(len(searchWord)):
-            sw += searchWord[i]
-            li = []
-            for pi in range(len(products)):
-                currName = products[pi]
-                diff = False
-                k = 0
-                if len(currName) <= i:
-                    diff = True
-                while k <= i and len(currName) > i:
-                    if currName[k] != sw[k]:
-                        diff = True
-                        break
-                    k += 1
-                if not diff and len(li) < 3:
-                    li.append(currName)
-            result.append(li)
+        # 2. 모든 검색글자에 대해 최대3개까지 조건에 맞는 단어를 보여줌
+        currSearchWord = ""
+        for ch in searchWord:
+            currList = []
+            currSearchWord += ch
+            # 정렬된 리스트에서 현재 검색글자가 들어갈만한 위치를 bisect로 구한다.
+            idx = bisect.bisect_left(products, currSearchWord) # idx까지가 검색가능한 단어의 첫 시작 idx라고 볼수 있다.
+            for p in products[idx:idx + 3]:
+                if p.startswith(currSearchWord):
+                    currList.append(p)
+            result.append(currList)
         return result
+            
+            
+            
